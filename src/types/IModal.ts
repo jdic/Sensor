64bc911a18408cd5be0a1b02eba29a44d79f584e
jdi <jmotadv@gmail.com>
2024-05-15 12:22:36 -0700

    [FIX] Optional properties

--- src/types/IModal.ts
+++ src/types/IModal.ts
@@ -3,9 +3,9 @@ import { IGeneric } from './IGeneric'
 export interface IModal extends IGeneric
 {
   open: boolean
-  onClose: () => void
-  title: string
-  children: React.ReactNode
+  onClose?: () => void
+  title?: string
+  children?: React.ReactNode
   disableCloseButton?: boolean
   description?: string
 }
