52e7c6f1e1807e73f06e7867e202c025bf4326c1
jdi <jmotadv@gmail.com>
2024-05-16 20:30:59 -0700

    [FIX] Adaption to useTemperature()

--- src/components/Sensor/Sensor.tsx
+++ src/components/Sensor/Sensor.tsx
@@ -1,19 +1,19 @@
 import { Field, Label, Description, Button, Fieldset } from '@headlessui/react'
-import { TrashIcon, PencilIcon } from '@heroicons/react/16/solid'
+import { useTemperature } from '../../store/useTemperature'
+import { TrashIcon } from '@heroicons/react/16/solid'
 import { useSensor } from '../../store/useSensor'
 import { ISensor } from '../../types/DSensor'
-import { useState } from 'react'
 
 export const Sensor: React.FC<ISensor> = ({ description, id, unit, api }) =>
 {
-  const [temperature, setTemperature] = useState<number>(0)
+  const { temperatures } = useTemperature()
   const { removeSensor } = useSensor()
 
   return (
     <div className='w-1/2 md:w-1/4 lg:w-1/6 px-2 py-2'>
       <Fieldset className='dark:bg-dark-primary w-full rounded-md p-4'>
         <Field className='flex items-center justify-between'>
-          <Label className='font-semibold dark:text-white'>PIN: <span className='font-medium'>{id}</span></Label>
+          <Label className='font-semibold dark:text-white'>PIN: <span className='font-medium'>{id ? id : '0'}</span></Label>
           <div className='flex items-center gap-1'>
             <Button className='size-5 p-[1px] rounded-md dark:bg-dark-accent' aria-description='Delete Sensor' onClick={() => removeSensor(id)}>
               <TrashIcon />
@@ -22,7 +22,7 @@ export const Sensor: React.FC<ISensor> = ({ description, id, unit, api }) =>
         </Field>
         <Field className='mt-2'>
           <div className='flex'>
-            <Label className='text-3xl font-medium dark:text-dark-secondary'>{temperature > 0 ? temperature : '~0'}</Label>
+            <Label className='text-3xl font-medium dark:text-dark-secondary'>{temperatures[id] || '~0'}</Label>
             <Label className=''>{unit}</Label>
           </div>
         </Field>
